e607249d74a3991985b58d53e25463298df2bfe5
John Patrick Paraon <johnpatrickparaon.dev@gmail.com>
2026-04-20 00:15:02 +0800

    Updated the flow of refresh token, I change it to check first if the token has been revoked already before checking if the user exist and then remove unnecessary refresh token service.

--- src/main/java/com/example/queue_management_system/service/RefreshTokenService.java
+++ src/main/java/com/example/queue_management_system/service/RefreshTokenService.java
@@ -6,5 +6,4 @@ public interface RefreshTokenService {
     void createRefreshToken(String refreshToken, String email);
     RefreshToken getRefreshToken (String refreshToken);
     void revokeToken(String refreshToken);
-    void deleteRefreshToken(String token);
 }
